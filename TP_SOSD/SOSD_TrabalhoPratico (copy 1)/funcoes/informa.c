03cf60f085e647b125cb14b2f2a581d68ca65b1b
ZeCosgrove <a18826@alunos.ipca.pt>
2020-12-22 11:35:29 +0000

    informa

--- TP_SOSD/SOSD_TrabalhoPratico (copy 1)/funcoes/informa.c
+++ TP_SOSD/SOSD_TrabalhoPratico (copy 1)/funcoes/informa.c
@@ -7,10 +7,7 @@
 
 int main(int argc, const char* argv[])
 {
-    int fd, leitura;
-	char buffer;
-	long int total = 0;
-	char* resultado;
+    int fd;
 	if(argc > 2){
 		write(STDERR_FILENO, "Too many arguments.\n", 21);
 		exit(EXIT_FAILURE);
@@ -18,24 +15,14 @@ int main(int argc, const char* argv[])
 		write(STDERR_FILENO, "Missing arguments.\n", 20);
 		exit(EXIT_FAILURE);
 	}
+
 	fd = open(argv[1], O_RDONLY);
 	/* Check file valid. If not, return error. */
 	if(fd < 0){
 		write(STDERR_FILENO, "Não foi possível abrir o ficheiro.", 37);
 		return WEXITSTATUS(fd);
 	}
-	/* Check character -> increment */
-	leitura = read(fd, &buffer, sizeof(buffer));
-	while(leitura > 0){
-		leitura = read(fd, &buffer, sizeof(buffer));
-		total++;
-	}
-	/* -1 houve erro */
-	if (leitura == -1) return 1;
-	itostring(total, &resultado);
-	write(STDOUT_FILENO, "Resultado: ", 12);
-	write(STDOUT_FILENO, resultado, strsize(resultado));
-	write(STDOUT_FILENO, "\n", 2);
-	close(fd);
+
+    
 	return 0;
 }
